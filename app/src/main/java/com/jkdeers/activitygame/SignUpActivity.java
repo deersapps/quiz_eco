f8799157444f478225ce57d9c858b9fc641b66d9
ASIM <asim.jan@6connex.com>
2022-04-24 16:03:58 +0530

    sign up

--- app/src/main/java/com/jkdeers/activitygame/SignUpActivity.java
+++ app/src/main/java/com/jkdeers/activitygame/SignUpActivity.java
@@ -4,58 +4,43 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.TextView;
 
-
-public class Signin extends AppCompatActivity implements View.OnClickListener {
-
-    TextView tvForgetPassword;
-    Button btnLogin;
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnSignUp;
-
+    Button btnLogIn;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
+        setContentView(R.layout.activity_sign_up);
+
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        init();
-    }
 
-    private void init() {
-        tvForgetPassword = findViewById(R.id.tvForgetPassword);
-        btnLogin = findViewById(R.id.btnLogin);
         btnSignUp = findViewById(R.id.btnSignUp);
+        btnLogIn = findViewById(R.id.btnLogin);
 
-        tvForgetPassword.setOnClickListener(this);
-        btnLogin.setOnClickListener(this);
         btnSignUp.setOnClickListener(this);
+        btnLogIn.setOnClickListener(this);
     }
 
     @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.tvForgetPassword:{
-                startActivity(new Intent(getApplicationContext(),SignUpActivity.class));
-                break;
-            }
             case R.id.btnLogin:{
-                startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
+                startActivity(new Intent(getApplicationContext(),Signin.class));
                 finish();
                 break;
             }
             case R.id.btnSignUp:{
-                startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
+                startActivity(new Intent(getApplicationContext(), Signin.class));
                 finish();
                 break;
             }
         }
     }
-
 }
