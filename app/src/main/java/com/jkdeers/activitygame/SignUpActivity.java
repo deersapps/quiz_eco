241270ed866f25853a12fe194107055da1d25f1e
ASIM <asim.jan@6connex.com>
2022-04-24 16:49:18 +0530

    auto dropdown
    auto dropdown

--- app/src/main/java/com/jkdeers/activitygame/SignUpActivity.java
+++ app/src/main/java/com/jkdeers/activitygame/SignUpActivity.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
@@ -17,8 +19,18 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        // get reference to the string array that we just created
 
+        String[] countries = getResources().getStringArray(R.array.countries);
+        // create an array adapter and pass the required parameter
+        // in our case pass the context, drop down layout , and array.
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.dropdown_item, R.id.textView, countries);
+        // get reference to the autocomplete text view
+        AutoCompleteTextView autoCompleteTextView = (AutoCompleteTextView)
+                findViewById(R.id.autoCompleteTextView);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        autoCompleteTextView.setAdapter(arrayAdapter);
 
         btnSignUp = findViewById(R.id.btnSignUp);
         btnLogIn = findViewById(R.id.btnLogin);
